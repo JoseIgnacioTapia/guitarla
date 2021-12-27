ecab076a961d66f073aa5bda8ceb1b2f35ad76bc
JoseIgnacioTapia <josignaciot@gmail.com>
2021-12-27 16:51:22 -0300

    Footer & Tienda, Blog Pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout pagina="Inicio">
       <h1>Desde Inicio</h1>
-      <Link href="/nosotros">Ir a nosotros</Link>
     </Layout>
   );
 }
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,9 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
 
 const Nosotros = () => {
   return (
     <Layout pagina="Nosotros">
       <h1>Desde Nosotros</h1>
-      <Link href="/">Ir a Inicio</Link>
     </Layout>
   );
 };
