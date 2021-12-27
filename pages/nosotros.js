5f2a193831175317523320c366a4d543a3f3ec2f
JoseIgnacioTapia <josignaciot@gmail.com>
2021-12-27 17:11:04 -0300

    Nosotros Page

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,9 +1,43 @@
 import Layout from '../components/Layout';
+import Image from 'next/image';
+import styles from '../styles/Nosotros.module.css';
 
 const Nosotros = () => {
   return (
     <Layout pagina="Nosotros">
-      <h1>Desde Nosotros</h1>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+
+        <div className={styles.contenido}>
+          <Image
+            layout="responsive"
+            width={600}
+            height={450}
+            src="/img/nosotros.jpg"
+            alt="Imagen sobre nosotros"
+          />
+          <div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Quibusdam, odio quisquam voluptatum ipsum aperiam numquam ex
+              perspiciatis cupiditate beatae est reprehenderit libero, expedita
+              veniam debitis sapiente unde reiciendis. Expedita ad voluptatibus
+              quos sapiente enim optio recusandae numquam, dignissimos, voluptas
+              necessitatibus qui illo iste fuga ratione rerum reiciendis sequi
+              impedit minima!
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Quibusdam, odio quisquam voluptatum ipsum aperiam numquam ex
+              perspiciatis cupiditate beatae est reprehenderit libero, expedita
+              veniam debitis sapiente unde reiciendis. Expedita ad voluptatibus
+              quos sapiente enim optio recusandae numquam, dignissimos, voluptas
+              necessitatibus qui illo iste fuga ratione rerum reiciendis sequi
+              impedit minima!
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
